e5c8cb9fcfb18acbb8a2427ce947ad7c6e29a051
JoakimKarlberg <31949808+JoakimKarlberg@users.noreply.github.com>
2017-11-02 11:02:37 +0100

    Fixed Callback function in Bouncer to add Patrons + added concurrentqueue for patrons

--- Lab6/Lab6/Bouncer.cs
+++ Lab6/Lab6/Bouncer.cs
@@ -4,33 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+
 namespace Lab6
 {
     public class Bouncer
     {
         private Action<string> Callback;
-
-        public string PatronName { get; set; }
-        int RemainingPatrons = 10;
+        private Action<Patron> PatronCallback;
+        Random random = new Random();
+        int maxPatrons = 10;
 
         List<string> PatronNameList = new List<string>()
             { "Jonas", "Klas", "Göran", "Getrud", "Daniel", "Petra", "Tor", "Styr-Björn", "Greta", "Livingston", "Margret" };
 
         //Work method
-        public void Work(Action<string> Callback)
+        public void Work(Action<string> Callback, Action<Patron> patronCallback)
         {
-            this.Callback = Callback;
+            Task.Run(() => {
+                DateTime timer = DateTime.Now;
+                this.PatronCallback = patronCallback;
+                this.Callback = Callback;
 
-            Random random = new Random();
-
-            while (RemainingPatrons >= 0)
-            {
-                Thread.Sleep(random.Next(1000, 5000));
-                // Här skapar vi patron-object som sedan skickan till queuen i main med hjälp av delegates. Vi behöver alltså inte göra nått med 
-                // understående rad med kod eftersom den bara skriver namnen till listboxen. 
-                Callback(PatronNameList[random.Next(PatronNameList.Count)]);
-                RemainingPatrons--;             
-            }
+                while (maxPatrons > 0)
+                {
+                    Thread.Sleep(random.Next(1000, 5000));
+                    string patronName = PatronNameList[random.Next(PatronNameList.Count)];
+                    Callback(patronName);
+                    patronCallback(new Patron(patronName));
+                    maxPatrons--;
+                }
+            });
         }
     }
 }
--- Lab6/Lab6/MainWindow.xaml.cs
+++ Lab6/Lab6/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.Concurrent;
+
 namespace Lab6
 {
     /// <summary>
@@ -21,6 +23,7 @@ namespace Lab6
     public partial class MainWindow : Window
     {
         private CancellationTokenSource cts = new CancellationTokenSource();
+        ConcurrentQueue<Patron> patronQueue = new ConcurrentQueue<Patron>();
 
         // Här deklarerar vi en ConcurrectQueue som patorns hamnar i när vi skapar dem. I main ska vi även ha funktioner som lägger till och
         // tar bort patrons från queuen. Med hjälp av delegates kan vi meddela de andra klasserna
@@ -35,21 +38,30 @@ namespace Lab6
             CancellationToken ct = cts.Token;
             BtnClose.IsEnabled = true;
             BtnStart.IsEnabled = false;
-
-            Task.Run(() =>
-            {
-                Bouncer b = new Bouncer();
-                b.Work(AddList);
-            });
-
+            
+            Bouncer b = new Bouncer();
+            b.Work(AddPatronList, AddPatronToQueue);
         }
+
         //Delegate funktion för Bouncer
-        private void AddList(string info)
+        private void AddPatronList(string info)
         {
             Dispatcher.Invoke(() => 
             {
-                ListPatron.Items.Insert(0, info);      
+                ListPatron.Items.Insert(0, $"{info} has entered the bar.");      
             });
+        }
+
+        //Function that adds Patron to ConcurrentQueue
+        private void AddPatronToQueue(Patron p)
+        {
+            patronQueue.Enqueue(p);
+        }
+
+        //Function that removes Patron from ConcurrentQueue
+        private void RemovePatronFromQueue(Patron p)
+        {
+            patronQueue.TryDequeue(out p);
         }
 
         private void BtnClose_Click(object sender, RoutedEventArgs e)
@@ -57,6 +69,11 @@ namespace Lab6
             cts.Cancel();
             BtnClose.IsEnabled = true;
             BtnStart.IsEnabled = false;
+        }
+
+        private void DebugPatron_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine(((Patron)patronQueue.Last()).Name);
         }
     }
 }
